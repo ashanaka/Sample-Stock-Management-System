ff2057c3a0042c7ebd85a211f28d868a729c0aa8
ashanaka <44833876+ashanaka@users.noreply.github.com>
2018-12-03 16:23:06 +0530

    Add files via upload

---
+++ StockManagementSystem/src/stockmanagementsystem/Items.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stockmanagementsystem;
+
+/**
+ *
+ * @author ashan
+ */
+public class Items {
+    int id;
+    String name;
+    int price;
+    int quantity;
+    int total;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+    
+}
